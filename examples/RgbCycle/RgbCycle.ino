4b3204a1eb70ed3e66976b29190053a1c3f4633e
Matt Shepcar <1621617+mattshepcar@users.noreply.github.com>
2020-07-04 03:31:33 +0100

    Reduced rgbcycle example program size by removing printf

--- examples/RgbCycle/RgbCycle.ino
+++ examples/RgbCycle/RgbCycle.ino
@@ -12,6 +12,16 @@ void setup()
         SmoothLedCcl::PB1_USART0_ASYNCCH1); // this pin will be an output but is only used for the clock signal
 }
 
+void printHexDigit(uint8_t value)
+{
+    Serial.write(value >= 10 ? value + 'A' - 10 : value + '0');
+}
+void printHex(uint8_t value)
+{
+    printHexDigit(value >> 4);
+    printHexDigit(value & 15);
+}
+
 void loop() 
 {
     wdt_reset();
@@ -20,7 +30,11 @@ void loop()
 
     // have to reinitialize Serial because SmoothLedCcl uses it to write LED data
     Serial.begin(115200);
-    Serial.printf("SmoothLed #%02x%02x%02x\n", r, g, b);
+    Serial.print("SmoothLed #");
+    printHex(r);
+    printHex(g);
+    printHex(b);
+    Serial.println();
     Serial.flush();
 
     leds.beginTransaction();
